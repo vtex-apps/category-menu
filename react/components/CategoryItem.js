966c23bb987341bf274c3dd87278278bbba3a60c
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-04-12 13:38:35 -0300

    Remove graphql query from CategoryItem

--- react/components/CategoryItem.js
+++ react/components/CategoryItem.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { graphql } from 'react-apollo'
 import classNames from 'classnames'
-
-import categoryQuery from '../queries/categoryQuery.gql'
-import LoadingBar from './LoadingBar'
 
 /**
  * Component that represents a single category displayed in the menu, also displays
@@ -12,7 +8,7 @@ import LoadingBar from './LoadingBar'
  */
 class CategoryItem extends Component {
   render() {
-    const { data: { category, loading } } = this.props
+    const { category } = this.props
 
     const wrapperClasses = classNames(
       'h-100 w-100 dib pl5 pr4 vtex-category-item black-90 hover-white hover-bg-black-90',
@@ -22,56 +18,42 @@ class CategoryItem extends Component {
     )
 
     return (
-      <div className="h3 w4">
-        <LoadingBar loading={loading}>
-          <div className={wrapperClasses}>
-            <a href={category.href} className="db mt6 no-underline ttu">
-              {category.name}
-            </a>
+      <div className="h3 w4" data-testid="category-item">
+        <div className={wrapperClasses}>
+          <a href={category.href} className="db mt6 no-underline ttu">
+            {category.name}
+          </a>
 
-            {category.hasChildren && (
-              <div className="vtex-category-sub-menu pv6 ph5 br2 br--bottom">
-                <ul className="list ma0 pa0 f6">
-                  {category.children.map(subCategory => (
-                    <li key={subCategory.id} className="lh-copy">
-                      <a
-                        className="near-black no-underline underline-hover"
-                        href={subCategory.href}
-                      >
-                        {subCategory.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </div>
-        </LoadingBar>
+          {category.hasChildren && (
+            <div className="vtex-category-sub-menu pv6 ph5 br2 br--bottom">
+              <ul className="list ma0 pa0 f6">
+                {category.children.map(subCategory => (
+                  <li key={subCategory.id} className="lh-copy">
+                    <a
+                      className="near-black no-underline underline-hover"
+                      href={subCategory.href}
+                    >
+                      {subCategory.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
 }
 
 CategoryItem.propTypes = {
-  /** Id of the category */
-  id: PropTypes.number.isRequired,
-  data: PropTypes.shape({
-    category: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      href: PropTypes.string.isRequired,
-      hasChildren: PropTypes.bool.isRequired,
-    }),
-    loading: PropTypes.bool.isRequired,
+  /** Category to be displayed */
+  category: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    href: PropTypes.string.isRequired,
+    hasChildren: PropTypes.bool.isRequired,
   }).isRequired,
 }
 
-const options = {
-  options: props => ({
-    variables: {
-      id: props.id,
-    },
-  }),
-}
-
-export default graphql(categoryQuery, options)(CategoryItem)
+export default CategoryItem
