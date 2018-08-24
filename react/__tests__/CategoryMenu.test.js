da371a2716f5ae544c9460926a328abbd5207c5b
Claudivan Filho <j.claudivan.filho@gmail.com>
2018-08-24 11:50:26 -0300

    Update rename CategoryMenu.js to index.js

--- react/__tests__/CategoryMenu.test.js
+++ react/__tests__/CategoryMenu.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { IntlProvider } from 'react-intl'
-import { CategoryMenuWithIntl } from '../CategoryMenu'
+import { CategoryMenuWithIntl } from '../index'
 
 describe('CategoryMenu component', () => {
   let wrapper
