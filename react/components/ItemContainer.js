d67a592edefdaa783db9466cb265c7b924aff8a4
Rerisson Daniel <rerisson.matos@splab.ufcg.edu.br>
2019-01-17 16:13:43 -0300

    Fix style issues

--- react/components/ItemContainer.js
+++ react/components/ItemContainer.js
@@ -76,7 +76,7 @@ export default class ItemContainer extends Component {
   render() {
     const { containerStyle } = this.props
     return (
-      <div className={`${categoryMenu.itemContainer} absolute w-100 left-0 w-100 bg-base pb2 bw1 bb b--muted-3`} style={containerStyle}>
+      <div className={`${categoryMenu.itemContainer} absolute w-100 left-0 bg-base pb2 bw1 bb b--muted-3`} style={containerStyle}>
         <ul className="w-100 w-90-l w-80-xl center ph3-s ph7-m ph6-xl flex">
           {this.props.categories.map(category => (
             <li key={category.id} className="dib pa2 w-20">
